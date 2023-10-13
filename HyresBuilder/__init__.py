530e16ba6ec985edf4f80bd3ce12ed8432be7325
Shanlong Li <shanlongli@umass.edu>
2023-10-13 16:32:09 -0400

    Update __init__.py

--- HyresBuilder/__init__.py
+++ HyresBuilder/__init__.py
@@ -7,6 +7,6 @@ The package consists of the following Python modules:
 * HyresBuilder
 * Geometry
 """
-__version__ = "1.2.5"
+__version__ = "1.2.6"
 from .HyresBuilder import *
 from .Geometry import *
