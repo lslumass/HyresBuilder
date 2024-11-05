6badbb0df7d1fc96c42b1779b4d872dfb26f38b5
lslumass <shanlongli@umass.edu>
2024-11-05 10:52:22 -0500

    update

---
+++ HyresBuilder/utils.py
@@ -0,0 +1,18 @@
+import importlib.resources as pkg_res
+
+def load_ff(model='protein'):
+    if model == 'protein':
+        path1 = pkg_res.path("HyresBuilder.forcefield", "top_hyres_GPU.inp")
+        path2 = pkg_res.path("HyresBuilder.forcefield", "param_hyres_GPU.inp")
+    elif model == 'RNA':
+        path1 = pkg_res.path("HyresBuilder.forcefield", "top_RNA.inp")
+        path2 = pkg_res.path("HyresBuilder.forcefield", "param_RNA.inp")
+    elif model == 'ATP':
+        path1 = pkg_res.path("HyresBuilder.forcefield", "top_ATP.inp")
+        path2 = pkg_res.path("HyresBuilder.forcefield", "param_ATP.inp")
+    else:
+        print("Error: The model type {} is not supported, choose from protein, RNA, ATP.".format(model))
+        exit(1)
+    
+    top_inp, param_inp = str(path1), str(path2)
+    return top_inp, param_inp
