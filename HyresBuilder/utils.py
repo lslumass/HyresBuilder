dfd3c3067cd840b3725c4cc80f0bb6d37cffb9dd
lslumass <shanlongli@umass.edu>
2024-11-05 11:37:47 -0500

    update

--- HyresBuilder/utils.py
+++ HyresBuilder/utils.py
@@ -1,18 +1,21 @@
 import pkg_resources as pkg_res
 
-def load_ff(model='protein'):
-    if model == 'protein':
-        path1 = pkg_res.resource_filename("HyresBuilder", "forcefield/top_hyres_GPU.inp")
-        path2 = pkg_res.resource_filename("HyresBuilder", "forcefield/param_hyres_GPU.inp")
-    elif model == 'RNA':
-        path1 = pkg_res.resource_filename("HyresBuilder", "top_RNA.inp")
-        path2 = pkg_res.resource_filename("HyresBuilder", "param_RNA.inp")
-    elif model == 'ATP':
-        path1 = pkg_res.resource_filename("HyresBuilder", "top_ATP.inp")
-        path2 = pkg_res.resource_filename("HyresBuilder", "param_ATP.inp")
-    else:
-        print("Error: The model type {} is not supported, choose from protein, RNA, ATP.".format(model))
-        exit(1)
-    
-    top_inp, param_inp = str(path1), str(path2)
+def load_ff(model=['protein',]):
+    top_inp, param_inp = [], []
+    for m in model:
+        if m == 'protein':
+            path1 = pkg_res.resource_filename("HyresBuilder", "forcefield/top_hyres_GPU.inp")
+            path2 = pkg_res.resource_filename("HyresBuilder", "forcefield/param_hyres_GPU.inp")
+        elif m == 'RNA':
+            path1 = pkg_res.resource_filename("HyresBuilder", "forcefield/top_RNA.inp")
+            path2 = pkg_res.resource_filename("HyresBuilder", "forcefield/param_RNA.inp")
+        elif m == 'ATP':
+            path1 = pkg_res.resource_filename("HyresBuilder", "forcefield/top_ATP.inp")
+            path2 = pkg_res.resource_filename("HyresBuilder", "forcefield/param_ATP.inp")
+        else:
+            print("Error: The model type {} is not supported, choose from protein, RNA, ATP.".format(model))
+            exit(1)
+        top_inp.append(str(path1))
+        param_inp.append(str(path2))
+
     return top_inp, param_inp
