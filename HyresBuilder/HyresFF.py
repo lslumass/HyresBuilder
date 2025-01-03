83481437971524cab2bebd1d32022e552eae401d
lslumass <shanlongli@umass.edu>
2025-01-03 10:14:41 -0500

    update

--- HyresBuilder/HyresFF.py
+++ HyresBuilder/HyresFF.py
@@ -646,7 +646,7 @@ def MixSystem(psf, system, ffs):
     eps_base = ffs['eps_base']
     scales = {'AA':1.0, 'AG':1.0, 'AC':0.8, 'AU':0.8, 'GA':1.0, 'GG':1.0, 'GC':0.8, 'GU':0.8,
               'CA':0.4, 'CG':0.4, 'CC':0.2, 'CU':0.4, 'UA':0.4, 'UG':0.4, 'UC':0.2, 'UU':0.2,
-              'A-U':0.80, 'C-G':1.11, 'G-G': 1.00}
+              'A-U':0.83, 'C-G':1.11, 'G-G': 1.00}
     # get all the groups of bases
     grps = []
     for atom in psf.topology.atoms():
