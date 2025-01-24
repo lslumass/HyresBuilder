453ffe16f61a0fcd70b998fc55fe2cf7489dce8d
lslumass <shanlongli@umass.edu>
2025-01-24 04:39:11 -0500

    update

--- HyresBuilder/HyresFF.py
+++ HyresBuilder/HyresFF.py
@@ -186,61 +186,35 @@ def iConRNASystem(psf, system, ffs):
     eps_base = ffs['eps_base']
     scales = {'AA':1.0, 'AG':1.0, 'AC':0.8, 'AU':0.8, 'GA':1.0, 'GG':1.0, 'GC':0.8, 'GU':0.8,
               'CA':0.4, 'CG':0.4, 'CC':0.2, 'CU':0.4, 'UA':0.4, 'UG':0.4, 'UC':0.2, 'UU':0.2,
-              'A-U':1.0, 'C-G':1.35}
-    # get all the groups of bases
-#    grps = []
-#    for atom in psf.topology.atoms():
-#        if atom.name == "NA":
-#            if atom.residue.name in ['A', 'G']:
-#                grps.append([atom.residue.name, [atom.index, atom.index+1]])
-#                grps.append([atom.residue.name, [atom.index+2, atom.index+3]])
-#            elif atom.residue.name in ['C', 'U']:
-#                grps.append([atom.residue.name, [atom.index, atom.index+1]])
-#                grps.append([atom.residue.name, [atom.index+1, atom.index+2]])
-#    # base stacking
-#    fstack = CustomCentroidBondForce(2, 'eps_stack*(5*(r0/r)^10-6.0*(r0/r)^6); r=distance(g1, g2);')
-#    fstack.setName('StackingForce')
-#    fstack.addPerBondParameter('eps_stack')
-#    fstack.addGlobalParameter('r0', 0.35*unit.nanometers)
-#    # add all group
-#    for grp in grps:
-#        fstack.addGroup(grp[1])
-#    # get the stacking pairs
-#    sps = []
-#    for i in range(0,len(grps)-2,2):
-#        grp = grps[i]
-#        pij = grps[i][0] + grps[i+2][0]
-#        sps.append([[i+1, i+2], scales[pij]*eps_base])
-#    for sp in sps:
-#        fstack.addBond(sp[0], [sp[1]])
-#    print('    add ', fstack.getNumBonds(), 'stacking pairs')
-#    system.addForce(fstack)
-
+              'A-U':0.83, 'C-G':1.11}
     # get all the groups of bases
     grps = []
     for atom in psf.topology.atoms():
         if atom.name == "NA":
             if atom.residue.name in ['A', 'G']:
-                grps.append([atom.residue.name, [atom.index, atom.index+1, atom.index+2, atom.index+3]])
+                grps.append([atom.residue.name, [atom.index, atom.index+1]])
+                grps.append([atom.residue.name, [atom.index+2, atom.index+3]])
             elif atom.residue.name in ['C', 'U']:
-                grps.append([atom.residue.name, [atom.index, atom.index+1, atom.index+2]])
+                grps.append([atom.residue.name, [atom.index, atom.index+1]])
+                grps.append([atom.residue.name, [atom.index+1, atom.index+2]])
     # base stacking
-    Aform = CustomCentroidBondForce(2, 'eps_stack*(5*(ra/r)^10-6.0*(ra/r)^6); r=distance(g1, g2);')
-    Aform.setName('AformStackingForce')
-    Aform.addPerBondParameter('eps_stack')
-    Aform.addGlobalParameter('ra', 0.37*unit.nanometers)
-
+    fstack = CustomCentroidBondForce(2, 'eps_stack*(5*(r0/r)^10-6.0*(r0/r)^6); r=distance(g1, g2);')
+    fstack.setName('StackingForce')
+    fstack.addPerBondParameter('eps_stack')
+    fstack.addGlobalParameter('r0', 0.34*unit.nanometers)
+    # add all group
     for grp in grps:
-        Aform.addGroup(grp[1])
+        fstack.addGroup(grp[1])
     # get the stacking pairs
     sps = []
-    for i in range(len(grps)-1):
-        pij = grps[i][0] + grps[i+1][0]
-        sps.append([[i, i+1], scales[pij]*eps_base])
+    for i in range(0,len(grps)-2,2):
+        grp = grps[i]
+        pij = grps[i][0] + grps[i+2][0]
+        sps.append([[i+1, i+2], scales[pij]*eps_base])
     for sp in sps:
-        Aform.addBond(sp[0], [sp[1]])
-    print('    add ', Aform.getNumBonds(), 'Aform stacking pairs')
-    system.addForce(Aform)
+        fstack.addBond(sp[0], [sp[1]])
+    print('    add ', fstack.getNumBonds(), 'stacking pairs')
+    system.addForce(fstack)
     
     # base pairing
     print('\n# add base pair force')
@@ -292,8 +266,8 @@ def iConRNASystem(psf, system, ffs):
                 c_p.append(int(atom.index))
     # add A-U pair through CustomHbondForce
     eps_AU = eps_base*scales['A-U']
-    r_au = 0.32*unit.nanometer
-    r_au2 = 0.43*unit.nanometer
+    r_au = 0.29*unit.nanometer
+    r_au2 = 0.37*unit.nanometer
     
     if num_A != 0 and num_U != 0:
         formula = f"""eps_AU*(5.0*(r_au/r)^10-6.0*(r_au/r)^6 + 5*(r_au2/r2)^10-6.0*(r_au2/r2)^6)*step(cos5)*cos5;
@@ -314,12 +288,12 @@ def iConRNASystem(psf, system, ffs):
         
     # add C-G pair through CustomHbondForce
     eps_CG = eps_base*scales['C-G']
-    r_cg = 0.32*unit.nanometer
-    r_cg2 = 0.39*unit.nanometer
+    r_cg = 0.29*unit.nanometer
+    r_cg2 = 0.35*unit.nanometer
     
     if num_C != 0 and num_G != 0:
         formula = f"""eps_CG*(5.0*(r_cg/r)^10-6.0*(r_cg/r)^6 + 5*(r_cg2/r2)^10-6.0*(r_cg2/r2)^6)*step(cos5)*cos5;
-                  r=distance(a1,d1); r2=distance(a3,d2); cos5=-cos(phi)^5; phi=angle(d1,a1,a2);
+                  r=distance(a1,d1); r2=distance(a3,d2); cos5=-cos(phi)^5; phi=angle(d1,a1,a2); psi=dihedral(a3,a1,d1,d2);
                   eps_CG={eps_CG.value_in_unit(unit.kilojoule_per_mole)};
                   r_cg={r_cg.value_in_unit(unit.nanometer)}; r_cg2={r_cg2.value_in_unit(unit.nanometer)}
                   """
--- HyresBuilder/utils.py
+++ HyresBuilder/utils.py
@@ -10,6 +10,9 @@ def load_ff(model='protein'):
     elif model == 'RNA':
         path1 = pkg_res.resource_filename("HyresBuilder", "forcefield/top_RNA.inp")
         path2 = pkg_res.resource_filename("HyresBuilder", "forcefield/param_RNA.inp")
+    elif model == 'RNA2':
+        path1 = pkg_res.resource_filename("HyresBuilder", "forcefield/top_RNA.inp")
+        path2 = pkg_res.resource_filename("HyresBuilder", "forcefield/param_RNA2.inp")
     elif model == 'RNA_mix':
         path1 = pkg_res.resource_filename("HyresBuilder", "forcefield/top_RNA_mix.inp")
         path2 = pkg_res.resource_filename("HyresBuilder", "forcefield/param_RNA_mix.inp")
@@ -17,7 +20,7 @@ def load_ff(model='protein'):
         path1 = pkg_res.resource_filename("HyresBuilder", "forcefield/top_ATP.inp")
         path2 = pkg_res.resource_filename("HyresBuilder", "forcefield/param_ATP.inp")
     else:
-        print("Error: The model type {} is not supported, choose from protein, protein_m, RNA, RNA_m, ATP.".format(model))
+        print("Error: The model type {} is not supported, choose from protein, protein_m, RNA, RNA2, RNA_m, ATP.".format(model))
         exit(1)
     top_inp = str(path1)
     param_inp = str(path2)
