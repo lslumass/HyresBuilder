92ae078b3d638532c72a3a912062e08ba4212ab5
lslumass <shanlongli@umass.edu>
2025-01-31 10:45:36 -0500

    update

--- scripts/position_restraint_example.py
+++ scripts/position_restraint_example.py
@@ -9,6 +9,22 @@ from openmm.unit import *
 from openmm.app import *
 from openmm import *
 import numpy as np
+import MDAnalysis as mda
+from MDAnalysis.analysis.dssp import DSSP
+
+
+# calculate the structure of each residue, and get the list of structured CA
+def identify_structured_residue(psf, pdb):
+    u=mda.Universe(psf, pdb)
+    sel = u.select_atoms('segname A0')
+    cas = u.select_atoms('segname A0 and name CA')
+    result = DSSP(sel).run()
+    strcutre_CA = []
+    for ca, s in zip(cas, result):
+        if s in ['E', 'H']:
+            strcutre_CA.append(ca.index)
+    
+    return strcutre_CA
 
 
 def position_restraint(psf, pdb, system):
