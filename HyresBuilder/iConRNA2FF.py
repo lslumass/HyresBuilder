f4c6d0e475b13ead7dd601ddd38533854f8c7f31
lslumass <shanlongli@umass.edu>
2025-02-19 06:15:41 -0500

    update

--- HyresBuilder/iConRNA2FF.py
+++ HyresBuilder/iConRNA2FF.py
@@ -92,29 +92,26 @@ def iConRNA2System(psf, system, ffs):
     for atom in psf.topology.atoms():
         if atom.name == "NA":
             if atom.residue.name in ['A', 'G']:
-                grps.append([atom.residue.name, [atom.index, atom.index+1]])
-                grps.append([atom.residue.name, [atom.index+2, atom.index+3]])
+                grps.append([atom.residue.name, [atom.index, atom.index+1, atom.index+2, atom.index+3]])
             elif atom.residue.name in ['C', 'U']:
-                grps.append([atom.residue.name, [atom.index, atom.index+1]])
-                grps.append([atom.residue.name, [atom.index+1, atom.index+2]])
+                grps.append([atom.residue.name, [atom.index, atom.index+1, atom.index+2]])
     # base stacking
-    fstack = CustomCentroidBondForce(2, 'eps_stack*((r0/r)^10-2*(r0/r)^5); r=distance(g1, g2);')
-    fstack.setName('IntraStackingForce')
-    fstack.addPerBondParameter('eps_stack')
-    fstack.addGlobalParameter('r0', 0.38*unit.nanometers)
-    # add all group
+    Aform = CustomCentroidBondForce(2, 'eps_stack*((ra/r)^10-2*(ra/r)^5); r=distance(g1, g2);')
+    Aform.setName('IntraStackingForce')
+    Aform.addPerBondParameter('eps_stack')
+    Aform.addGlobalParameter('ra', 0.40*unit.nanometers)
+
     for grp in grps:
-        fstack.addGroup(grp[1])
+        Aform.addGroup(grp[1])
     # get the stacking pairs
     sps = []
-    for i in range(0,len(grps)-2,2):
-        grp = grps[i]
-        pij = grps[i][0] + grps[i+2][0]
-        sps.append([[i+1, i+2], scales[pij]*eps_base])
+    for i in range(len(grps)-1):
+        pij = grps[i][0] + grps[i+1][0]
+        sps.append([[i, i+1], scales[pij]*eps_base])
     for sp in sps:
-        fstack.addBond(sp[0], [sp[1]])
-    print('    add ', fstack.getNumBonds(), 'stacking pairs')
-    system.addForce(fstack)
+        Aform.addBond(sp[0], [sp[1]])
+    print('    add ', Aform.getNumBonds(), 'Aform stacking pairs')
+    system.addForce(Aform)
     
     # base pairing
     print('\n# add base pair force')
