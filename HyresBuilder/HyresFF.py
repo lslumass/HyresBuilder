c83c4f0c8fce146f9d8da4ee443e7d3f4e807bea
lslumass <shanlongli@umass.edu>
2025-03-13 08:46:17 -0400

    update

--- HyresBuilder/HyresFF.py
+++ HyresBuilder/HyresFF.py
@@ -745,9 +745,9 @@ def MixSystem(psf, system, ffs):
     # base stakcing and paring
     # define relative strength of base pairing and stacking
     eps_base = ffs['eps_base']
-    scales = {'AA':1.0, 'AG':1.0, 'AC':0.8, 'AU':0.8, 'GA':1.0, 'GG':1.0, 'GC':0.8, 'GU':0.8,
-              'CA':0.4, 'CG':0.4, 'CC':0.2, 'CU':0.4, 'UA':0.4, 'UG':0.4, 'UC':0.2, 'UU':0.2,
-              'A-U':0.85, 'C-G':1.20, 'G-G': 1.00}
+    scales = {'AA':1.0, 'AG':1.0, 'AC':0.8, 'AU':0.8, 'GA':1.2, 'GG':1.2, 'GC':0.7, 'GU':0.8,
+              'CA':0.4, 'CG':0.4, 'CC':0.3, 'CU':0.4, 'UA':0.4, 'UG':0.4, 'UC':0.2, 'UU':0.2,
+              'A-U':0.9, 'C-G':1.25, 'G-G': 1.00}
     # get all the groups of bases
     grps = []
     for atom in psf.topology.atoms():
