60711fcc54bee6fd08fc70383f6abf7e9d233b9d
lslumass <shanlongli@umass.edu>
2025-09-15 14:15:43 -0400

    update

--- HyresBuilder/utils.py
+++ HyresBuilder/utils.py
@@ -209,3 +209,141 @@ def setup(model, args, dt, pressure=1*unit.atmosphere, friction=0.1/unit.picosec
     sim.context.setVelocitiesToTemperature(temperture)
     print(f'Langevin, CUDA, {temperture}')
     return system, sim
+
+
+def setup2(model, args, dt, lmd, pressure=1*unit.atmosphere, friction=0.1/unit.picosecond, gpu_id="0"):
+    """
+    Set up the simulation system with given parameters.
+    Parameters:
+    -----------
+    model: str
+        The type of model to use ('protein', 'RNA', 'mix').
+    args: argparse.Namespace
+        The command line arguments containing simulation parameters.
+    dt: float
+        The time step for the integrator.
+    pressure: unit.Quantity
+        The pressure for the MonteCarloBarostat (default is 1 atm).
+    friction: unit.Quantity
+        The friction coefficient for the Langevin integrator (default is 0.1 / ps).
+    gpu_id: str
+        The GPU device index to use (default is "0").
+    Returns:
+    --------
+    system: openmm.System
+        The constructed OpenMM system.
+    sim: openmm.app.Simulation
+        The OpenMM simulation object.
+    """
+
+    print('\n################## set up simulation parameters ###################')
+    # 1. input parameters
+    pdb_file = args.pdb
+    psf_file = args.psf
+    T = args.temp
+    c_ion = args.salt/1000.0                                   # concentration of ions in M
+    c_Mg = args.Mg                                           # concentration of Mg in mM
+    ensemble = args.ens
+    lmd = args.lmd
+    
+    # 2. set pbc and box vector
+    if ensemble == 'non' and c_Mg != 0.0:
+        print("Error: Mg ion cannot be usde in non-periodic system.")
+        exit(1)
+    if ensemble in ['NPT', 'NVT']:
+        # pbc box length
+        if len(args.box) == 1:
+            lx, ly, lz = args.box[0], args.box[0], args.box[0]
+        elif len(args.box) == 3:
+            lx = args.box[0]
+            ly = args.box[1]
+            lz = args.box[2]
+        else:
+            print("Error: You must provide either one or three values for box.")
+            exit(1)
+        a = Vec3(lx, 0.0, 0.0)
+        b = Vec3(0.0, ly, 0.0)
+        c = Vec3(0.0, 0.0, lz)
+    elif ensemble not in ['NPT', 'NVT', 'non']:
+        print("Error: The ensemble must be NPT, NVT or non. The input value is {}.".format(ensemble))
+        exit(1)
+    
+    # 3. force field parameters
+    temperture = T*unit.kelvin 
+    er_t = cal_er(T)                                                   # relative electric constant
+    if model == 'protein':
+        er_t = er_t*20/77.6
+    dh = cal_dh(c_ion, T)                                            # Debye-Huckel screening length in nm
+    ffs = {
+        'temp': T,                                                  # Temperature
+        'lmd': lmd,                                                  # Charge scaling factor of P-
+        'dh': dh,                                                  # Debye Huckel screening length
+        'ke': 138.935456,                                           # Coulomb constant, ONE_4PI_EPS0
+        'er': er_t,                                                  # relative dielectric constant
+    }
+
+    # 4. load force field files
+    if model == 'protein':
+        top_pro, param_pro = load_ff('protein')
+        params = CharmmParameterSet(top_pro, param_pro)
+    elif model == 'RNA':
+        top_RNA, param_RNA = load_ff('RNA')
+        params = CharmmParameterSet(top_RNA, param_RNA)
+    elif model == 'mix':
+        top_RNA, param_RNA = load_ff('RNA_mix')
+        top_pro, param_pro = load_ff('protein_mix')
+        params = CharmmParameterSet(top_RNA, param_RNA, top_pro, param_pro)
+    else:
+        print("Error: Only 'protein', 'RNA', and 'mix' models are supported.")
+        exit(1)
+
+    print('\n################## load coordinates and topology ###################')
+    # 5. import coordinates and topology form charmm pdb and psf
+    pdb = PDBFile(pdb_file)
+    psf = CharmmPsfFile(psf_file)
+    top = psf.topology
+    if ensemble == 'non':
+        system = psf.createSystem(params, nonbondedMethod=CutoffNonPeriodic, constraints=HBonds)
+    else:
+        psf.setBox(lx, ly, lz)
+        top.setPeriodicBoxVectors((a, b, c))
+        top.setUnitCellDimensions((lx, ly,lz))
+        system = psf.createSystem(params, nonbondedMethod=CutoffPeriodic, constraints=HBonds)
+        system.setDefaultPeriodicBoxVectors(a, b, c)
+
+    # 6. construct force field
+    print('\n################## build system ###################')
+    if model == 'protein':
+        system = HyresSystem(psf, system, ffs)
+        print('This is a Hyres protein system')
+    elif model == 'RNA':
+        system = iConRNASystem(psf, system, ffs)
+        print('This is a iConRNA system')
+    elif model == 'mix':
+        system = MixSystem(psf, system, ffs)
+        print('This is a Hyres protein + iConRNA mixed system')
+    else:
+        print("Error: Only 'protein', 'RNA', and 'mix' models are supported. The input value is {}.".format(model))
+        exit(1)
+
+    # 7. set simulation
+    print('\n################### prepare simulation ####################')
+    if ensemble == 'NPT':
+        print('This is a NPT system')
+        system.addForce(MonteCarloBarostat(pressure, temperture, 25))
+    elif ensemble == 'NVT':
+        print('This is a NVT system')
+    elif ensemble == 'non':
+        print('This is a non-periodic system')
+    else:
+        print("Error: The ensemble must be NPT, NVT or non. The input value is {}.".format(ensemble))
+        exit(1)
+
+    integrator = LangevinMiddleIntegrator(temperture, friction, dt)
+    plat = Platform.getPlatformByName('CUDA')
+    prop = {'Precision': 'mixed', 'DeviceIndex': gpu_id}
+    sim = Simulation(top, system, integrator, plat, prop)
+    sim.context.setPositions(pdb.positions)
+    sim.context.setVelocitiesToTemperature(temperture)
+    print(f'Langevin, CUDA, {temperture}')
+    return system, sim
