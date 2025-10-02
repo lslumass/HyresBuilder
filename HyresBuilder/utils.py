52408b564f762f5ca3e4d88e2f0629a853109e7c
lslumass <shanlongli@umass.edu>
2025-10-02 17:18:04 -0400

    update

--- HyresBuilder/utils.py
+++ HyresBuilder/utils.py
@@ -247,7 +247,7 @@ def setup2(args, dt, lmd=0, pressure=1*unit.atmosphere, friction=0.1/unit.picose
     dh = cal_dh(c_ion, T)                                            # Debye-Huckel screening length in nm
     # Mg-P interaction
     lmd = args.Mg
-    print(f'er: {er_t}, dh: {dh}, lmd: {lmd}')
+    print(f'er: {er}, dh: {dh}, lmd: {lmd}')
     ffs = {
         'temp': T,                                                  # Temperature
         'lmd': lmd,                                                  # Charge scaling factor of P-
