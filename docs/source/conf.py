448e4d684fca16613dcb6e8831b1a59cbdc28d56
lslumass <shanlongli@umass.edu>
2026-03-10 15:03:29 -0400

    readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,14 +1,9 @@
 import os
 import sys
+import builtins
 from unittest.mock import MagicMock
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
+# 1. Setup the mocks
 MOCK_MODULES = [
     'psfgen',
     'openmm',
@@ -23,20 +18,22 @@ MOCK_MODULES = [
 for mod in MOCK_MODULES:
     sys.modules[mod] = MagicMock()
 
-# Make 'unit' available as a name since utils.py uses it in default args
-import openmm.unit as unit  # this now points to the MagicMock
-sys.modules['__main__'].unit = sys.modules['openmm.unit']
+# 2. Specifically handle the 'unit' name error
+# Create a mock for unit and inject it into builtins
+mock_unit = MagicMock()
+builtins.unit = mock_unit
 
+# 3. Ensure the project path is correct
+# Since your build log shows HyresBuilder is in the root:
 sys.path.insert(0, os.path.abspath('../..'))
 
+# -- Project information -----------------------------------------------------
 project = 'HyresBuilder'
 copyright = '2026, Shanlong Li, Jian Huang, Yumeng Zhang, Jianhan Chen'
 author = 'Shanlong Li, Jian Huang, Yumeng Zhang, Jianhan Chen'
 release = 'latest'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
@@ -46,10 +43,7 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = []
 
-
-
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
 html_theme = 'sphinx_rtd_theme'
+# Make sure this directory actually exists in docs/source/
 html_static_path = ['_static']
