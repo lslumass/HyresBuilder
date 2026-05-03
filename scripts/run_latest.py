0cea9698e76a5ba93e91dfc53806b6bf35249eb0
lslumass <shanlongli@umass.edu>
2026-05-03 16:06:27 -0400

    running_script

--- scripts/run_latest.py
+++ scripts/run_latest.py
@@ -24,7 +24,6 @@ parser.add_argument('-t', "--temp", default=303, type=float, help="system temper
 parser.add_argument('-b', "--box", nargs='+', type=float, help="box dimensions in nanometer, e.g., '50 50 50' ")
 parser.add_argument('-s', "--salt", default=150.0, type=float, help="salt concentration in mM, default is 150.0 mM")
 parser.add_argument('-e', "--ens", default='NVT', type=str, help="simulation ensemble, NPT, NVT, or non, non is for non-periodic system")
-parser.add_argument('-m', "--Mg", default=0.0, type=float, help="Mg2+ concentration in mM")
 params = parser.parse_args()
 out = params.out
 
