376123530ad629e58273ad6964ca32d4bedc84d6
lslumass <shanlongli@umass.edu>
2026-05-12 20:04:20 -0400

    utils

--- HyresBuilder/utils.py
+++ HyresBuilder/utils.py
@@ -150,7 +150,8 @@ def estimate_lmd(cNa, cMg, length, Rg, T):
     nMg = 0.47*(Na_Mg/(Na_Mg+cNa))
 
     nMg_T = nMg + 0.0012*(T-273-30)
-    lmd = 1.265*(nMg_T/0.172)**0.625/(1+(nMg_T/0.172)**0.625)
+    # lmd = 1.265*(nMg_T/0.172)**0.625/(1+(nMg_T/0.172)**0.625)           # for er = 20.0
+    lmd = 1.493*(nMg_T/0.172)**0.625/(1+(nMg_T/0.172)**0.625)           # for er = 60.0
 
     return lmd
 
